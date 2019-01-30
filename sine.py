d51e9a035f1b76a1987becfa96424fa2a901ca41
VYSHNAVI123 <47188178+VYSHNAVI123@users.noreply.github.com>
2019-01-30 22:48:13 +0530

    Add files via upload

---
+++ sine.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt;
+import numpy as np;
+f1=int(input("enter the frequency"));
+fs=int(input("enter the sample frequency"));
+n=int(input("enter the no of samples"));
+t=np.arange(n)
+y=np.sin(2*np.pi*t*f1/fs)
+plt.xlabel('time period')
+plt.ylabel('amplitude')
+plt.subplot(3,1,3)
+plt.stem(t,y)
+plt.show()
+
+
